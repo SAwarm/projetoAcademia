fb7daad971df1a8479a82c884ed5f7aefcde7d86
SAwarm <jonasdasilvaelias@gmail.com>
2021-10-06 21:09:07 -0300

    função buscar dados do funcionário adicionada

---
+++ backend/buscarFuncionarios.php
@@ -0,0 +1,28 @@
+<?php
+
+    include('./../backend/conexao.php');
+            
+    if (!$link) {
+    	die('Não foi possível conectar: ' . mysqli_error());
+    }
+
+    $q = $_POST['q'];
+    
+    $query = "SELECT * from funcionario where nome LIKE '%$q%'";
+    
+    $result = mysqli_query ($link, $query);
+
+    while($row = mysqli_fetch_array($result)) {
+        $rows [] = $row;
+    }
+    if($result != false){
+        $result_sql = mysqli_fetch_array($result);
+    }
+
+    if(!empty($rows)){
+        echo json_encode($rows);
+    }else{
+        echo "null";
+    }
+        
+?>
--- telas/cadastro_funcionario.php
+++ telas/cadastro_funcionario.php
@@ -24,9 +24,7 @@
 
 <div class="container" style="margin-top: 5%; margin-bottom: 5%;">
   <div class="row">
-    <div class="col-sm-12">
-    
-
+    <div class="col-sm-6">
      <h2>Cadastro de funcionários</h2><br><br>
      <form action=".././backend/cadastroFuncionario.php" method="POST" enctype="multipart/form-data">
         <fieldset>
@@ -111,6 +109,34 @@
         </fieldset>
       </form>
    </div>
+   <div class="col-sm-6">
+   <input class="text-buscar"></input>
+   <?php
+		//include('./../backend/conexao.php');
+		
+		//if (!$link) {
+		//	die('Não foi possível conectar: ' . mysqli_error());
+		//}
+		
+		//$query = "SELECT * from endereco LIMIT 10";
+		//$result = mysqli_query ($link, $query);
+
+	?>
+	<table class="table">
+		<thead>
+			<tr>
+			<th scope="col">ID</th>
+			<th scope="col">Nome</th>
+			</tr>
+		</thead>
+		<?php //if ($result) { 
+			//while($row = mysqli_fetch_array($result)) { ?>
+				<tbody id="popularDados">
+				</tbody>
+			<?php// } ?>
+		<?php //} ?>
+	</table>
+   </div>
   </div>
 </div>
 
@@ -165,6 +191,33 @@
 		$('.modalMensagem').modal('show');
     });
 
+	$( ".text-buscar" ).keyup(function() {
+		$.ajax({
+          url: ".././backend/buscarFuncionarios.php",
+          type: "POST",
+          data: {q : $('.text-buscar').val()},
+          success: function(result){
+			cols = "";
+			if(result == "null"){
+				cols += '<td scope="row"></td>';
+				$("#popularDados").html(cols);
+			}else{
+				jq_json_obj = $.parseJSON(result);
+				cont = jq_json_obj.length
+				for (x = 0; x < cont; x++){
+					cols += '<tr><td scope="row">teste</td>';
+					cols += '<td>aahah</td></tr>';
+
+					$("#popularDados").html(cols);
+				} 
+			}
+
+          },
+          error: function(error){
+            console.log(error);
+          }
+      });
+	});
 
   </script>
 </html>
