1cfa2040b5677d8ed8738effd82fd736857a4177
SAwarm <jonasdasilvaelias@gmail.com>
2022-02-16 21:11:08 -0300

    FIX: Spaces

--- backend/buscarClientes.php
+++ backend/buscarClientes.php
@@ -1,38 +1,9 @@
 <?php
 
-    /*include('./../backend/conexao.php');
-            
-    if (!$link) {
-    	die('Não foi possível conectar: ' . mysqli_error());
-    }
-
-    $q = $_POST['q'];
-    
-    $query = "SELECT * from cliente where nome LIKE '%$q%'";
-    
-    $result = mysqli_query ($link, $query);
-
-    while($row = mysqli_fetch_array($result)) {
-        $rows [] = $row;
-    }
-    if($result != false){
-        $result_sql = mysqli_fetch_array($result);
-    }
-
-    if(!empty($rows)){
-        if($q == ""){
-            echo "null";
-        }else{
-            echo json_encode($rows);
-        }
-    }else{
-        echo "null";
-    }*/
-
     include('./conexao.php');
 
     if (!$link) {
-    	die('Não foi possível conectar: ' . mysqli_error());
+    	die('Não foi possível conectar: ' . mysqli_error($link));
     }
 
     $action = $_POST['action'];
