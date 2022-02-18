8ba8ae115511590652cd689111ac934437e0f2ba
SAwarm <jonasdasilvaelias@gmail.com>
2022-02-17 21:32:58 -0300

    ADD: vendor past

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '3a6cf2d5d214f195927d23cc612e2d0cf757321b',
+        'name' => 'projetoacademia/projetoacademia',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'projetoacademia/projetoacademia' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '3a6cf2d5d214f195927d23cc612e2d0cf757321b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
