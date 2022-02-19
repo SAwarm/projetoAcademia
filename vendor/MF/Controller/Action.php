a5a87dc1ad32c0cc64e03037eba8c0263bcbd55b
SAwarm <jonasdasilvaelias@gmail.com>
2022-02-18 22:40:11 -0300

    ADD: init vendor files

---
+++ vendor/MF/Controller/Action.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace MF\Controller;
+
+abstract class Action {
+
+    protected $view;
+
+    public function __construct()
+    {
+        $this->view = new \stdClass;
+    }
+
+    public function render($view, $layout = 'layout')
+    {
+        $this->view->page = $view;
+
+        if (file_exists("./../App/Views/".$layout.".phtml")) {
+            require_once "./../App/Views/".$layout.".phtml";
+        }
+
+        return $this->content();
+    }
+
+    protected function content()
+    {
+        $actual_class = get_class($this);
+
+        $actual_class = str_replace('App\\Controllers\\', '', $actual_class);
+
+        $actual_class = strtolower(str_replace('Controller', '', $actual_class));
+
+        require_once "./../App/Views/".$actual_class."/".$this->view->page.".phtml";
+    }
+}
