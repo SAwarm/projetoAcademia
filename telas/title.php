009adec9b6de17fb3d640b13f7afdc8b933942a4
SAwarm <jonasdasilvaelias@gmail.com>
2022-02-21 22:02:36 -0300

    DELETE: title

--- telas/title.php
+++
@@ -1,5 +0,0 @@
-<div class="jumbotron jumbotron-fluid text-center" style="background-color: #2F4F4F; margin-bottom:0">
-    <h1 style=" color: #FFF;
-     -webkit-text-stroke: 1px #FFF;
-      text-stroke: 1px #FFF;">Jr Fitness system</h1>
-</div>
