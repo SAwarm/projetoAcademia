214dcedeb4dee57fdbe08aadc3baef3c5292c13f
SAwarm <jonasdasilvaelias@gmail.com>
2022-02-23 22:19:57 -0300

    FIX: tags

--- telas/login.php
+++ telas/login.php
@@ -1,17 +1,3 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-  <title>Login</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <script src="./node_modules/jquery/dist/jquery.js"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" type="text/css" href="estilos.css"/>
-</head>
-
 <body>
   <?php include('telas/title.php'); ?>
 
@@ -85,4 +71,3 @@
       });
     });
   </script>
-</html>
