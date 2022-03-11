d9fadd875bcc9b6c325e11d88c0bac4b618454fd
SAwarm <jonasdasilvaelias@gmail.com>
2022-03-10 22:46:23 -0300

    FIX: index controller

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -15,33 +15,4 @@ class indexController extends Action
 
         $this->render('index');
     }
-
-    public function register()
-    {
-        $user = Container::getModel('User');
-
-        $user->__set('name', $_POST['name']);
-        $user->__set('email', $_POST['email']);
-        $user->__set('password', md5($_POST['password']));
-
-        if (
-            $user->validationRegister() &&
-            count($user->getUserEmail()) <= 0
-        ) {
-
-            $user->store();
-
-            return $this->render('register');
-        }
-
-        $this->view->user = array(
-            'name'       =>  $_POST['name'],
-            'email'      =>  $_POST['email'],
-            'password'   =>  $_POST['password'],
-        );
-
-        $this->view->errorRegister = true;
-
-        return $this->render('subscribe');
-    }
 }
