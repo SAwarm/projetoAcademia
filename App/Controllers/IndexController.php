745843ddefea40ab1a2900ae0ac328d9ca4eb5c5
SAwarm <jonasdasilvaelias@gmail.com>
2022-03-14 22:32:41 -0300

    FIX: index controller

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -3,6 +3,8 @@
 namespace App\Controllers;
 
 //* recurses
+
+use App\Controllers\Auth\Logged;
 use App\Controllers\RulesView\RulesViewController;
 use MF\Model\Container;
 use MF\Controller\Action;
@@ -12,6 +14,8 @@ class indexController extends Action
     public function index()
     {
         RulesViewController::getTitle($this, 'Login - Jr Fitness System');
+
+        Logged::validatedLogged();
 
         $this->render('index');
     }
