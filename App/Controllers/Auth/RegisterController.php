27aa84ed0ce8e8640aeb8b9017f699f67b2461a3
SAwarm <jonasdasilvaelias@gmail.com>
2022-03-14 22:32:19 -0300

    FIX: controller users

--- App/Controllers/Auth/RegisterController.php
+++ App/Controllers/Auth/RegisterController.php
@@ -7,10 +7,8 @@ use App\Controllers\RulesView\RulesViewController;
 use MF\Model\Container;
 use MF\Controller\Action;
 
-
 class RegisterController extends Action
 {
-
     public function register()
     {
         return 'hello!';
